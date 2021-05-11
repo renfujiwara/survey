5478c87fcad364e83fb4c67e0774a8fd683d0c85
renfujiwara <82931379+renfujiwara@users.noreply.github.com>
2021-05-11 17:49:44 +0900

    Update StatStream.py

--- StatStream.py
+++ StatStream.py
@@ -1,1 +1,15 @@
+%matplotlib inline
 
+import functools
+import matplotlib.pyplot as plt
+import cmath
+import random
+
+import numpy as np
+
+S = list()
+
+def DFT(s,b,w,k)
+  crr_digest = 0
+  retyrn crr_digest
+  
